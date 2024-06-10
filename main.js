2b31d2a79f1f19cb6e0caaade5afcf1988c97830
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-06-10 23:47:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+// for  loop 
+let person = "my name is anaya";
+for (let I = 0; I <= 100; I++) {
+    //is sy variable print hoga br br
+    console.log(person);
+    // is sy number counting print hogi
+    console.log(I + " " + person);
+}
+// while loop
+let i = 0;
+while (i = 100) {
+    console.log(i, 'hello');
+    i++;
+}
+// boolen loop
+let input = false;
+while (input == false) {
+    console.log("hello");
+}
